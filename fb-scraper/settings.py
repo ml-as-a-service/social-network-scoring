2056a54f0af7578ab148514476c727baa08379c3
Mario Garcia <mariogarcia.ar@gmail.com>
2021-01-16 20:24:13 -0300

    agrego un utilitario para levantar los post de las redes sociales. Issue #9 task 3

---
+++ fb-scraper/settings.py
@@ -0,0 +1,24 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+# User credentials
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+# TODO: analizar como podemos hacer funcionar los browsers dentro de los contenedores
+
+# Required binaries
+BROWSER_EXE = '/usr/bin/firefox'
+GECKODRIVER = '/usr/bin/geckodriver'
+FIREFOX_BINARY = FirefoxBinary(BROWSER_EXE)
+
+#  Code to disable notifications pop up of Chrome Browser
+PROFILE = webdriver.FirefoxProfile()
+# PROFILE.DEFAULT_PREFERENCES['frozen']['javascript.enabled'] = False
+PROFILE.set_preference("dom.webnotifications.enabled", False)
+PROFILE.set_preference("app.update.enabled", False)
+PROFILE.update_preferences()
